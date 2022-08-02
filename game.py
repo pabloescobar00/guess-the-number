87dec974a47cbcf16afd3c700299a559a64854e6
pabloescobar00 <paw111cio@gmail.com>
2022-08-02 14:32:48 +0100

    Update game.py

--- game.py
+++ game.py
@@ -3,7 +3,8 @@ from random import randint
 
 los=randint(0,20)
 odp=-1
-i=00
+i=0
+money=0
 
 while odp!=los:
     odp=int(input("Give the answer"))
@@ -12,5 +13,5 @@ while odp!=los:
         print("The drawn number is greater than your answer")
     elif odp>los:
         print("The drawn number is less than your answer")
-
-print("Congratulations, you guessed the number", "Your number of attempts: ", i)
+money+=1
+print("Congratulations, you guessed the number", "Your number of attempts: ", i, "Your money: ", money)
